33907dfcf365d90f2f7e739c0883ecb48003ef19
Fernando Méndez <jmendez@itexico.net>
2017-12-05 15:44:42 -0600

    Lesson 2 - OddOccurrenceInArray (25% performance)

---
+++ Codility.Training/OddOccurrencesInArray/OddOccurrenceInArray.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Training.OddOccurrencesInArray
+{
+    public static class OddOccurrenceInArray
+    {
+        public static int Solution(int[] array)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int a in array)
+            {
+                if (result.Contains(a))
+                {
+                    result.Remove(a);
+                }
+                else
+                {
+                    result.Add(a);
+                }
+            }
+
+            return result.FirstOrDefault();
+        }
+    }
+}
---
+++ Codility.UnitTest/TOddOccurrenceInArray/TOddOccurrenceInArray.cs
@@ -0,0 +1,39 @@
+﻿using Codility.Training.OddOccurrencesInArray;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.UnitTest.TOddOccurrenceInArray
+{
+    [TestClass]
+    public class TOddOccurrenceInArray
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] input = { 9, 3, 9, 3, 9, 7, 9 };
+            int expected = 7;
+            int result = OddOccurrenceInArray.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] input = { 1, 2, 3, 4, 5, 6, 2, 4, 6, 1, 3 };
+            int expected = 5;
+            int result = OddOccurrenceInArray.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int[] input = { 1, 3, 1 };
+            int expected = 3;
+            int result = OddOccurrenceInArray.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
