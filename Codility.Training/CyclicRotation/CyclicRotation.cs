61d04b97313f50fd47eafde55cd1bcff03e7f993
Fernando Méndez <jmendez@itexico.net>
2017-12-05 17:19:27 -0600

    Lesson 2 - CyclicRotation

---
+++ Codility.Training/CyclicRotation/CyclicRotation.cs
@@ -0,0 +1,31 @@
+﻿namespace Codility.Training.CyclicRotation
+{
+    public static class CyclicRotation
+    {
+        public static int[] Solution(int[] array, int moves)
+        {
+            int tempA = 0, tempB = 0;
+            int total = array.Length;
+
+            for (int i = 0; i < moves; i++)
+            {
+                for (int j = 0; j < total; j++)
+                {
+                    if (j == 0)
+                    {
+                        tempA = array[j];
+                        array[j] = array[total - 1];
+                    }
+                    else 
+                    {
+                        tempB = array[j];
+                        array[j] = tempA;
+                        tempA = tempB;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Codility.UnitTest/TCyclicRotation/TCyclicRotation.cs
@@ -0,0 +1,43 @@
+﻿using Codility.Training.CyclicRotation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Codility.UnitTest.TCyclicRotation
+{
+    [TestClass]
+    public class TCyclicRotation
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int moves = 3;
+            int[] input = { 3, 8, 9, 7, 6 };
+            int[] expected = { 9, 7, 6, 3, 8 };
+            int[] result = CyclicRotation.Solution(input, moves);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int moves = 1;
+            int[] input = { 0, 0, 0 };
+            int[] expected = { 0, 0, 0 };
+            int[] result = CyclicRotation.Solution(input, moves);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int moves = 4;
+            int[] input = { 1, 2, 3, 4 };
+            int[] expected = { 1, 2, 3, 4 };
+            int[] result = CyclicRotation.Solution(input, moves);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
