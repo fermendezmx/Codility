d484b3900267e843f46576cc630a51fff2face32
Fernando Méndez <jmendez@itexico.net>
2017-12-05 18:37:03 -0600

    Lesson 3 - TapeEquilibrium (0% in performance - 2nd try)

--- Codility.Training/TapeEquilibrium/TapeEquilibrium.cs
+++ Codility.Training/TapeEquilibrium/TapeEquilibrium.cs
@@ -1,4 +1,7 @@
-﻿namespace Codility.Training.TapeEquilibrium
+﻿using System;
+using System.Linq;
+
+namespace Codility.Training.TapeEquilibrium
 {
     public static class TapeEquilibrium
     {
@@ -6,32 +9,18 @@
         {
             int N = A.Length;
             int splits = N - 1;
-
-            int result = 0;
+            int[] first, second;
+            int firstPart, secondPart, difference, result = 0;
 
             for (int P = 1; P <= splits; P++)
             {
-                int firstPart = 0, secondPart = 0;
+                first = A.Take(P).ToArray();
+                second = A.Skip(P).ToArray();
 
-                for (int i = 0; i < N; i++)
-                {
-                    if(i < P)
-                    {
-                        firstPart += A[i];
-                    }
-                    else
-                    {
-                        secondPart += A[i];
-                    }
-                }
-
-                int difference = firstPart - secondPart;
+                firstPart = first.Sum();
+                secondPart = second.Sum();
+                difference = Math.Abs(firstPart - secondPart);
                 
-                if(difference < 0)
-                {
-                    difference *= -1;
-                }
-
                 if(difference < result || P == 1)
                 {
                     result = difference;
@@ -40,5 +29,47 @@
 
             return result;
         }
+
+        #region 0% in performance
+
+        //public static int Solution(int[] A)
+        //{
+        //    int N = A.Length;
+        //    int splits = N - 1;
+        //    int result = 0;
+
+        //    for (int P = 1; P <= splits; P++)
+        //    {
+        //        int firstPart = 0, secondPart = 0;
+
+        //        for (int i = 0; i < N; i++)
+        //        {
+        //            if(i < P)
+        //            {
+        //                firstPart += A[i];
+        //            }
+        //            else
+        //            {
+        //                secondPart += A[i];
+        //            }
+        //        }
+
+        //        int difference = firstPart - secondPart;
+
+        //        if(difference < 0)
+        //        {
+        //            difference *= -1;
+        //        }
+
+        //        if(difference < result || P == 1)
+        //        {
+        //            result = difference;
+        //        }
+        //    }
+
+        //    return result;
+        //}
+
+        #endregion
     }
 }
