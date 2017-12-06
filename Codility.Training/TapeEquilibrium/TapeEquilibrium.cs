e8329a6b7de32f6ba995331088b45d404ed301b2
Fernando Méndez <jmendez@itexico.net>
2017-12-05 18:55:49 -0600

    Refactoring TapeEquilibrium

--- Codility.Training/TapeEquilibrium/TapeEquilibrium.cs
+++ Codility.Training/TapeEquilibrium/TapeEquilibrium.cs
@@ -9,19 +9,16 @@ namespace Codility.Training.TapeEquilibrium
         {
             int N = A.Length;
             int splits = N - 1;
-            int[] first, second;
-            int firstPart, secondPart, difference, result = 0;
+            int firstPart, secondPart, 
+                difference, result = 0;
 
             for (int P = 1; P <= splits; P++)
             {
-                first = A.Take(P).ToArray();
-                second = A.Skip(P).ToArray();
-
-                firstPart = first.Sum();
-                secondPart = second.Sum();
+                firstPart = A.Take(P).Sum();
+                secondPart = A.Skip(P).Sum();
                 difference = Math.Abs(firstPart - secondPart);
-                
-                if(difference < result || P == 1)
+
+                if (difference < result || P == 1)
                 {
                     result = difference;
                 }
