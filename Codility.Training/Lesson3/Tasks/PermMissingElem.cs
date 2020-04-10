6368048b6d05a6271576545f9597bee5a6f93ab2
Fernando Méndez <mendezc.jf@gmail.com>
2020-04-10 17:38:53 -0500

    Solution to problem: PermMissingElem

---
+++ Codility.Training/Lesson3/Tasks/PermMissingElem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Codility.Training.Lesson3.Tasks
+{
+    public static class PermMissingElem
+    {
+        public static int Solution(int[] A)
+        {
+            int N = A.Length;
+            int result = N + 1;
+
+            Array.Sort(A);
+
+            for (int i = 0; i < N; i++)
+            {
+                if (A[i] != i + 1)
+                {
+                    result = i + 1;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Codility.UnitTest/Lesson3/TPermMissingElem.cs
@@ -0,0 +1,29 @@
+﻿using Codility.Training.Lesson3.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.UnitTest.Lesson3
+{
+    [TestClass]
+    public class TPermMissingElem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] input = { 2, 3, 1, 5 };
+            int expected = 4;
+            int result = PermMissingElem.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] input = { 8, 9, 4, 3, 1, 5, 2, 6 };
+            int expected = 7;
+            int result = PermMissingElem.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
