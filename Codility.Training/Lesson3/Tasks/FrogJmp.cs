225abbc1bb2628bc8826adbc8572722948f57c49
Fernando Méndez <mendezc.jf@gmail.com>
2020-04-10 16:59:54 -0500

    Solving problem: FrogJmp

---
+++ Codility.Training/Lesson3/Tasks/FrogJmp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codility.Training.Lesson3.Tasks
+{
+    public static class FrogJmp
+    {
+        public static int Solution(int X, int Y, int D)
+        {
+            return (int)Math.Ceiling((double)(Y - X) / D);
+        }
+    }
+}
---
+++ Codility.UnitTest/Lesson3/TFrogJmp.cs
@@ -0,0 +1,33 @@
+﻿using Codility.Training.Lesson3.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.UnitTest.Lesson3
+{
+    [TestClass]
+    public class TFrogJmp
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int X = 10;
+            int Y = 85;
+            int D = 30;
+            int expected = 3;
+            int result = FrogJmp.Solution(X, Y, D);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int X = 2;
+            int Y = 245;
+            int D = 8;
+            int expected = 31;
+            int result = FrogJmp.Solution(X, Y, D);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
