f65e9625e5727430399cf72326f52bf04665b444
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-05 21:39:02 -0500

    [Solved] Lesson 4: FrogRiverOne

---
+++ Codility.Training/Lesson4/Tasks/FrogRiverOne.cs
@@ -0,0 +1,31 @@
+﻿namespace Codility.Training.Lesson4.Tasks
+{
+    public static class FrogRiverOne
+    {
+        public static int Solution(int X, int[] A)
+        {
+            int result = -1;
+            int counter = 0;
+            int n = A.Length;
+            int[] positions = new int[X + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                if (positions[A[i]] == 0)
+                {
+                    counter++;
+                }
+
+                positions[A[i]]++;
+
+                if (counter == X)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Codility.UnitTest/Lesson4/TFrogRiverOne.cs
@@ -0,0 +1,42 @@
+﻿using Codility.Training.Lesson4.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.UnitTest.Lesson4
+{
+    [TestClass]
+    public class TFrogRiverOne
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 5;
+            int[] input = { 1, 3, 1, 4, 2, 3, 5, 4 };
+            int expected = 6;
+            int result = FrogRiverOne.Solution(x, input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int x = 5;
+            int[] input = { 3 };
+            int expected = -1;
+            int result = FrogRiverOne.Solution(x, input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int x = 3;
+            int[] input = { 1, 3, 1, 3, 2, 1 };
+            int expected = 4;
+            int result = FrogRiverOne.Solution(x, input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
