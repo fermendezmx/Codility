94fc3bf95fdf5e2acd030fc53e6b5796027d22cf
Fernando Méndez <mendezc.jf@gmail.com>
2020-07-05 22:20:04 -0500

    [Solved] Lesson 4: PermCheck

---
+++ Codility.Training/Lesson4/Tasks/PermCheck.cs
@@ -0,0 +1,46 @@
+﻿namespace Codility.Training.Lesson4.Tasks
+{
+    public static class PermCheck
+    {
+        public static int Solution(int[] A)
+        {
+            int result = 0;
+            int counter = 0;
+            int n = A.Length;
+            int[] positions = new int[n + 1];
+
+            for (int i = 0; i < n; i++)
+            {
+                // Validate that A[i] is in range of 1 to 'n'
+                if (A[i] > n)
+                {
+                    // If it is out of range is not a permutation
+                    break;
+                }
+
+                // Indicates that we found the element on A[i]
+                positions[A[i]]++;
+
+                // The element should be just once
+                if (positions[A[i]] == 1)
+                {
+                    // So we count every time an element is found
+                    counter++;
+                    continue;
+                }
+
+                // The element is not only once so we quit
+                break;
+            }
+
+            // If all the elements were found
+            if (counter == n)
+            {
+                // Then is a permutation
+                result = 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Codility.UnitTest/Lesson4/TPermCheck.cs
@@ -0,0 +1,29 @@
+﻿using Codility.Training.Lesson4.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.UnitTest.Lesson4
+{
+    [TestClass]
+    public class TPermCheck
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] input = { 4, 1, 3, 2 };
+            int expected = 1;
+            int result = PermCheck.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] input = { 4, 1, 3 };
+            int expected = 0;
+            int result = PermCheck.Solution(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
